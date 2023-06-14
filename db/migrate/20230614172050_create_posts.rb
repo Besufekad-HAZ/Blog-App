2f2c779c0d72ac101b93b4fa5aa6ea4b9dfe533a
Besufekad-HAZ <besurye@gmail.com>
2023-06-14 20:42:16 +0300

    Create and run the necessary migration files

---
+++ db/migrate/20230614172050_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.references :author, null: false, foreign_key: { to_table: :users, on_delete: :cascade }
+      t.string :title
+      t.text :text
+      t.integer :comments_counter, default: 0
+      t.integer :likes_counter, default: 0
+      t.timestamps
+    end
+  end
+end

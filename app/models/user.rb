f81d3c6ef1726c30b503df32f3b261af20753e55
Besufekad-HAZ <besurye@gmail.com>
2023-06-15 00:20:59 +0300

    Update All models according to the instruction

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
 
   attribute :posts_counter, :integer, default: 0
+  validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_three_posts
     posts.order(created_at: :desc).limit(3)

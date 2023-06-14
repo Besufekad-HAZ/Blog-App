cb4bb07a0644190c1b728321df2c30e5816425c2
Besufekad-HAZ <besurye@gmail.com>
2023-06-14 23:55:53 +0300

    Update model post and user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,15 @@
 class Post < ApplicationRecord
-  belongs_to :author
+  belongs_to :author, class_name: 'User'
+  has_many :likes
+  has_many :comments
+
+  after_save :update_posts_count
+
+  def update_posts_count
+    author.increment!(:posts_counter)
+  end
+
+  def recent_five_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_many :likes, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
+  has_many :posts, foreign_key: 'author_id'
+
+  attribute :posts_counter, :integer, default: 0
+
+  def recent_three_posts
+    posts.order(created_at: :desc).limit(3)
+  end
 end

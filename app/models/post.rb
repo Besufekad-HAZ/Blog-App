bd0ce103241e03cf2d8f7a40757f076f24df6f37
Besufekad-HAZ <besurye@gmail.com>
2023-06-15 11:22:20 +0300

    Add private in posts.rb models and check all comments with rails c

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,11 +9,13 @@ class Post < ApplicationRecord
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def update_posts_count
-    author.increment!(:posts_counter)
-  end
-
   def recent_five_comments
     comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def update_posts_count
+    author.increment!(:posts_counter)
   end
 end

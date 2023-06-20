430f17c84670c4f0dd1d5211931b05d8fd3ca0e4
Besufekad-HAZ <besurye@gmail.com>
2023-06-20 13:04:41 +0300

    Add the route file to show user and posts

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users, only: [:index, :show] do
+   resources :posts, only: [:index, :show]
+  end
 end

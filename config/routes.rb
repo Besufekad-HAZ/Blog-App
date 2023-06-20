e393c8e6c56e05595ac506d845e5d3e9f922d16c
Besufekad-HAZ <besurye@gmail.com>
2023-06-20 13:11:43 +0300

    Update root page in routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'pages#blog'
+  root 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

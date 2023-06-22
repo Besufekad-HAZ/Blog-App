92a38281e7c1df212b809f6e678f3b804fb7de22
Besufekad-HAZ <besurye@gmail.com>
2023-06-22 10:53:12 +0300

    Add more tests in user_request_spec

--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -18,4 +18,20 @@ require 'rails_helper'
        expect(response.body).to include('<h1>Here is a list of all users</h1>')
      end
    end
+   context 'GET /show' do
+    it 'renders a successful response' do
+      get users_url(5)
+      expect(response).to be_successful
+    end
+
+    it 'renders the correct template' do
+      get user_url(5)
+      expect(response).to render_template('show')
+    end
+
+    it 'includes correct placeholder text' do
+      get user_url(5)
+      expect(response.body).to include('<h1>Here is a user for a given ID:</h1>')
+    end
+  end
  end

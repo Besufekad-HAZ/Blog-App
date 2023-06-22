14875e644043724759cefd0b5cab306044ded6bf
Besufekad-HAZ <besurye@gmail.com>
2023-06-22 19:48:01 +0300

    Add 3 users in seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+first_user = User.create(name: 'Versatile', photo: 'https://avatars.githubusercontent.com/u/80094659?v=4', bio: 'Mastering things Everyday.')
+second_user = User.create(name: 'Yidne', photo: 'https://avatars.githubusercontent.com/u/104775335?v=4', bio: 'The potential for coding and playing the saxophone is unlimited')
+third_user = User.create(name: 'Bese', photo: 'https://avatars.githubusercontent.com/u/23375153?v=4', bio: 'Your time is limited, so don't waste it living someone else's life')

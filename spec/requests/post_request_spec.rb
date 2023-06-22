a99cc5b9f05816650b197597bf723bc9b3b25c89
Besufekad-HAZ <besurye@gmail.com>
2023-06-22 10:45:23 +0300

    Update post_request_spec.rb file

--- spec/requests/post_request_spec.rb
+++ spec/requests/post_request_spec.rb
@@ -14,7 +14,23 @@ require 'rails_helper'
 
      it 'includes correct placeholder text' do
        get user_posts_path(1)
-       expect(response.body).to include('<h1>Here is a list of posts</h1>')
+       expect(response.body).to include('<h1>Here are the list of posts</h1>')
      end
    end
+    context 'GET /show' do
+    it 'renders a successful response' do
+      get user_post_url(1, 3)
+      expect(response).to be_successful
+    end
+
+    it 'renders the correct template' do
+      get user_post_url(1, 3)
+      expect(response).to render_template('show')
+    end
+
+    it 'includes correct placeholder text' do
+      get user_post_url(1, 3)
+      expect(response.body).to include('<h1>Here are the list of posts for a given user</h1>')
+    end
+  end
  end

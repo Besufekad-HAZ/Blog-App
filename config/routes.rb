4fd4f40c5901eb83bbb6aae575d133f2cdbbb7f1
Besufekad-HAZ <besurye@gmail.com>
2023-06-24 21:41:07 +0300

    Add comment button and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'users#index'
+  root "users#index"
   resources :users, only: [:index, :show] do
-   resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
+    end
   end
 end

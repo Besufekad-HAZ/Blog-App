10b8d230ea926af74ca40993cb17c3a64b98ba07
Besufekad-HAZ <besurye@gmail.com>
2023-06-24 22:13:10 +0300

    Fix rubocop linter errors

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,15 @@
 class LikesController < ApplicationController
-    def create
-      @post = params[:post_id]
-      @user = current_user
-      @like = Like.new(author_id: @user.id, post_id: @post)
+  def create
+    @post = params[:post_id]
+    @user = current_user
+    @like = Like.new(author_id: @user.id, post_id: @post)
 
-      if @like.save
-        flash[:success] = 'You liked the post.'
-      else
-        flash.now[:error] = 'Error'
-      end
-
-      redirect_to user_posts_path(@user.id, @post)
+    if @like.save
+      flash[:success] = 'You liked the post.'
+    else
+      flash.now[:error] = 'Error'
     end
+
+    redirect_to user_posts_path(@user.id, @post)
   end
+end

622e1fb3725fe6a46abc44cc4443a28cbd20f383
Besufekad-HAZ <besurye@gmail.com>
2023-06-27 13:09:41 +0300

    Update controller and posts index file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+    @post = Post.includes(:comments).find(params[:id])
     @comments = @post.comments
     @like = Like.new
   end

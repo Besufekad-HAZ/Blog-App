f1c9faa224aeb041d31501d57fe3f2cf1c75276f
Kelvin <ngotho.kelvin@yahoo.com>
2023-06-27 18:18:59 +0300

    Add Identation

--- spec/views/posts/index_spec.rb
+++ spec/views/posts/index_spec.rb
@@ -5,13 +5,22 @@ RSpec.describe 'Post index page', type: :feature do
   let!(:users) { [first_user] }
   let!(:posts) { Post.all }
 
-  first_user = User.create(name: 'Kelvin Kaviku', photo: 'https://avatars.githubusercontent.com/u/104892694?v=4',
-                           bio: 'I am a Full-Stack Developer with a strong passion for coding. Currently enrolled and learning at Microverse. I am open to new job opportunities.')
+  first_user = User.create(
+    name: 'Kelvin Kaviku',
+    photo: 'https://avatars.githubusercontent.com/u/104892694?v=4',
+    bio: 'I am a Full-Stack Developer with a strong passion for coding. Currently enrolled and learning at Microverse.
+    I am open to new job opportunities.'
+  )
 
-  first_post = Post.create(author_id: first_user.id, title: 'Hello', text: 'ONE This is my first post')
-  second_post = Post.create(author_id: first_user.id, title: 'Holaaa', text: 'TWO This is my second post')
+  first_post = Post.create(author_id: first_user.id,
+                           title: 'Hello',
+                           text: 'ONE This is my first post')
+  second_post = Post.create(author_id: first_user.id,
+                            title: 'Holaaa',
+                            text: 'TWO This is my second post')
 
-  first_comment = Comment.create(author_id: first_user.id, post_id: first_post.id,
+  first_comment = Comment.create(author_id: first_user.id,
+                                 post_id: first_post.id,
                                  text: 'Hello, this is my first comment.')
 
   before do

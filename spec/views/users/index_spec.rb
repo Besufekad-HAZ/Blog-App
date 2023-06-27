e83f1c50e45f106e741ffada64b7ef1baeb6790d
Besufekad-HAZ <besurye@gmail.com>
2023-06-27 17:20:34 +0300

    Update Posts Index spec and add user index spec files

---
+++ spec/views/users/index_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'User index page', type: :feature do
+  let!(:user) { User.create(name: '', bio: 'Coding', photo: 'https://avatars.githubusercontent.com/u/80094659?v=4') }
+  let!(:post) { Post.create(title: 'Post one', text: 'Content for post one', author: user) }
+
+  before do
+    visit users_path
+  end
+
+  describe 'contents of user index page' do
+    it 'should display username of each user' do
+      expect(page).to have_content(user.name)
+    end
+    it 'should display profile picture of each user' do
+      expect(page).to have_css("img[src='#{user.photo}']")
+    end
+    it 'should display number of posts each user has' do
+      expect(page).to have_content("Number of posts: #{user.posts.count}")
+    end
+  end
+  describe 'Links on user index page' do
+    it 'When clicking on user should go to user show page' do
+      click_link(user.name)
+      expect(page).to have_current_path(user_path(user))
+    end
+  end
+end

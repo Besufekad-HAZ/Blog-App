3b4defd7fdf26aa0280eb3467cf317da322a012c
Besufekad-HAZ <besurye@gmail.com>
2023-06-29 19:29:04 +0300

    Update forms and other labels in views/devise

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  devise_scope :user do
+ devise_scope :user do
     get '/users/sign_out', to: 'devise/sessions#destroy'
     root to: 'devise/sessions#new'
   end
